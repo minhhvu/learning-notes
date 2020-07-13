9f264cab5965b5edf55b37a342957adde558d70c
minhh <minhhaivuhd@gmail.com>
2020-07-13 11:08:19 -0400

    modules in Typescript

---
+++ typescript/modules/exporting_module.ts
@@ -0,0 +1,17 @@
+/*
+    Modules
+ */
+
+//Export
+
+export interface xInteface {} //export a declaration
+class YClass {}              //export statements
+export {YClass}
+export {YClass as NewYClass}  //Re-export with the name changed
+
+
+//Default export
+//export default myObject = {}; //equivalent to export = myObject
+
+
+
---
+++ typescript/modules/importing_module.ts
@@ -0,0 +1,4 @@
+//Importing
+import {xInteface} from "./exporting_module";
+import {YClass, NewYClass} from "./exporting_module";
+//or import * from "./exporting_module"
