bfc72fe39a6b240b5e87927463da531f877121fd
minhh <minhhaivuhd@gmail.com>
2020-07-19 14:22:27 -0400

    outline the application of TS on React/redux

---
+++ typescript/React Redux integration with Typescript/topics.ts
@@ -0,0 +1,28 @@
+/**
+ * Integrate Typescript with React/Redux
+ * - Props
+ * - State
+ * - Functional component
+ * - Class-based component
+ * - Component life-cycle
+ * - HOOK
+ * - Context, provider
+ *
+ * Integrate Typescript with Redux
+ * - Action
+ * - Action creator
+ * - Reducer
+ * - Async Actions
+ * - Middleware
+ *
+ * Advanced Topics:
+ * - Error Handler
+ * - Authentication
+ *
+ * Reference:
+ * https://www.pluralsight.com/guides/centralized-error-handing-with-react-and-redux
+ * React+TypeScript Cheatsheet: a comprehensive guide to using React with TypeScript
+ * https://github.com/typescript-cheatsheets/react-typescript-cheatsheet
+ * React + Redux in TypeScript Guide: extensive information on patterns for using React and Redux with TypeScript
+ * https://github.com/piotrwitek/react-redux-typescript-guide
+ */
